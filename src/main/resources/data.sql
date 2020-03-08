9113d0850cf615ba9328167b8c24d84850bf32dd
kiran <kiranr84@gmail.com>
2020-03-08 01:53:54 -0700

    Initial Commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into employee values(101,'1990-01-01','2010-02-02','Elizabeth','Warren','I','INACTIVE')
-insert into employee values(102,'1990-01-01','2010-02-02','Bernie','Sanders','M','ACTIVE')
-insert into employee values(103,'1990-01-01','2010-02-02','Joe','Biden','F','ACTIVE')
+insert into employee values(101,'1982-01-19','2011-02-02','Pete','Buttigieg','P','INACTIVE')
+insert into employee values(102,'1941-09-09','2007-01-03','Bernie','Sanders','','ACTIVE')
+insert into employee values(103,'1942-11-20','1973-01-03','Joe','Biden','R','ACTIVE')
